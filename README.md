22ded42851937f6dad134b252bff1104d1efef62
huberpa4 <22phuber@gmail.com>
2020-05-28 22:19:02 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# docker-compose-ghost-blog
-Docker-compose for standalone ghost setup with mariadb, nginx, certbot using let's encrypt (SSL/TLS)
+# Docker compose starter kit with ghost blog, nginx, mariadb and certbot (SSL/TLS)
+
+A [docker compose](https://docs.docker.com/compose/) starter kit for the [Ghost blogging platform](https://github.com/TryGhost/Ghost) features:
+- SSL/TLS (HTTPS) using [certbot](https://certbot.eff.org/) with automated renewal
+- [nginx](https://www.nginx.com/) as fast and reliable webserver
+- [mariadb](https://mariadb.org/) as database.
+
+Additional features:
+- Support for [Disqus](https://disqus.com/) comments
+
+## Requirements
+You need a computer or server with
+- Docker installed
+- Internet access
+- Ingress traffic on ports `80` and `443`
+
+## Getting started
+
+The current setup supports a `domain` name and an `alternative domain` name with `www.` prefix out of the box. But you can customize all settings.
+
+1. Copy `.env.template` to `.env`
+2. Change `.env` file and add **your values**. Also add, remove environment variables if needed.
+3. Optional configurations:
+   1. Edit the `docker-compose.yml` file: Change, add and remove environment variables if needed.
+   2. Edit the `certbot/files/docker-entrypoint.sh` file to use `--staging` and/or `--debug` for the initial certbot certificate creation.
+   3. If you migrate from another ghost blog: Export the content in the old admin panel. Copy existing blog data into `data/ghost/` and import content in the new admin panel.
+5. Check the configuration: `docker-compose config`
+6. Build the docker images: `docker-compose build --no-cache`
+7. Run the docker compse setup in daemon mode in the backgroud: `docker-compose up -d`
+
+Check logs: `docker-compose logs -f`
---
+++ ghost/files/docker-entrypoint.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+# Enables Disqus comments when ${DISQUS_SHORTNAME} is set
+# for the casper theme
+# https://ghost.org/integrations/disqus/
+function disqusCasperSetup ()
+{
+    declare -r casperPostFile="${GHOST_CONTENT}/themes/casper/post.hbs"
+    declare -r disqusSnippetFile="${GHOST_INSTALL}/disqus.txt";
+    local REPLACE;
+    local CONTENT;
+    # clean up when function is "exited"
+    trap 'rm "${disqusSnippetFile}"' RETURN
+    # Don't indent this heredoc section
+read -r -d '' REPLACE <<'EOF'
+            {{\!--
+            <section class="post-full-comments">
+                If you want to embed comments, this is a good place to do it!
+            </section>
+            --}}
+EOF
+
+    if [[ -n "${DISQUS_SHORTNAME}" && -f "${casperPostFile}" && -f "${disqusSnippetFile}" ]]; then
+        REPLACE=$(echo "${REPLACE}" | sed -e ':a' -e 'N' -e '$!ba' -e 's#\n#\\n#g')
+        CONTENT=$(sed -e ':a' -e 'N' -e '$!ba' -e 's#\n#\\n#g' "${disqusSnippetFile}")
+        echo "Setup disqus for shortname \"${DISQUS_SHORTNAME}\""
+        sed -i -e ':a' -e 'N' -e '$!ba' -e "s#${REPLACE}#${CONTENT}#" "${casperPostFile}"
+    fi
+    return 0
+}
+
+# allow the container to be started with `--user`
+if [[ "$*" == node*current/index.js* ]] && [ "$(id -u)" = '0' ]; then
+    find "${GHOST_CONTENT}" \! -user node -exec chown node '{}' +
+    disqusCasperSetup
+    exec su-exec node "${BASH_SOURCE[0]}" "$@"
+fi
+
+if [[ "$*" == node*current/index.js* ]]; then
+    baseDir="${GHOST_INSTALL}/content.orig"
+    for src in "$baseDir"/*/ "$baseDir"/themes/*; do
+        src="${src%/}"
+        target="${GHOST_CONTENT}/${src#$baseDir/}"
+        mkdir -p "$(dirname "$target")"
+        if [ ! -e "$target" ]; then
+            tar -cC "$(dirname "$src")" "$(basename "$src")" | tar -xC "$(dirname "$target")"
+        fi
+    done
+    disqusCasperSetup
+fi
+
+exec "$@"
